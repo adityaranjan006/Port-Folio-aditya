01b26e1bbea517307bd407c1afcf564138b1d5fe
Aditya Ranjan <101415127+adityaranjan006@users.noreply.github.com>
2023-06-01 17:21:54 +0530

    Update Ball.jsx
    Ball speed rotation

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -14,7 +14,7 @@ const Ball = (props) => {
   const [decal] = useTexture([props.imgUrl]);
 
   return (
-    <Float speed={1} rotationIntensity={1} floatIntensity={2}>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight color={'pink'} intensity={0.25} />
       <directionalLight position={[0, 0, 0.05]} />
       <mesh castShadow receiveShadow scale={2.75}>
@@ -27,7 +27,7 @@ const Ball = (props) => {
         />
         <Decal
           position={[0, 0, 1]}
-          rotation={[2.5 * Math.PI, 0, 6.25]}
+          rotation={[2 * Math.PI, 0, 6.25]}
           scale={1}
           map={decal}
           flatShading
