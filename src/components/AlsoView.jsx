f845e96219679d6f1a7e3ac968b118fbc3c03e9d
Aditya Ranjan <101415127+adityaranjan006@users.noreply.github.com>
2023-06-07 19:34:12 +0530

    Update AlsoView.jsx
    "GeeksForGeeks Footer is added"

--- src/components/AlsoView.jsx
+++ src/components/AlsoView.jsx
@@ -12,8 +12,10 @@ const AlsoView = () => {
           <ul className="list-none sm:flex flex-row gap-10">
             <li><a href="https://leetcode.com/1si20cs006/" target="_blank" className={styles.sectionSubText}>Leetcode</a></li>
             <li><a href="https://www.codechef.com/users/adi44" target="_blank"className={styles.sectionSubText}>Codechef</a></li>
-            <li><a href="https://github.com/adityaranjan006" target="_blank"className={styles.sectionSubText}>GitHub</a></li>
+            <li><a href="https://auth.geeksforgeeks.org/user/ranjanal5mf" target="_blank"className={styles.sectionSubText}>GeeksForGeeks</a></li>
             <li><a href="https://www.codingninjas.com/codestudio/profile/814978c9-fb22-4b12-aef4-e236c9fb0195" target="_blank"className={styles.sectionSubText}>CodeStudio</a></li>
+            <li><a href="https://github.com/adityaranjan006" target="_blank"className={styles.sectionSubText}>GitHub</a></li>
+            
           </ul>
           </div>
         </div>
